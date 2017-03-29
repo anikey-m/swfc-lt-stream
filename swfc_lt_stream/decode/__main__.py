d316a8019c7214fb4a9b092c78d3224a079cced3
Andrey Ovchinnikov <anikey.m@gmail.com>
2017-03-30 01:35:46 +0400

    Actually add decode script

---
+++ swfc_lt_stream/decode/__main__.py
@@ -0,0 +1,10 @@
+from swfc_lt_stream import cli, decode
+
+
+if __name__ == '__main__':
+    parser = cli.decoder_parser()
+    conf = parser.parse_args()
+
+    decoder = decode.Decoder(conf)
+    client = decode.Listener(conf.host, conf.port, decoder)
+    client.listen()
